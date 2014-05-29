7b945dab3bfd1c7b65e393c2c430f93876a72c3c
skaras <skaras@student.agh.edu.pl>
2014-05-29 16:26:07 +0200

    KRS refactored for Session usage, + example.

--- crawl/krs.py
+++ crawl/krs.py
@@ -2,113 +2,139 @@
 # -*- coding: utf8 -*-
 # coding=UTF8
 
-import requests, codecs, sys
+import requests, codecs, sys, re
 
-from lxml import etree
+from session    import Session
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+
+
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
 
 
 class KRS(object):
 
+    path_search_result = "./body/div[@id='all']/div[@id='main']/p/a"
+    path_data_table    = "./body/div[@id='all']/div[@id='main']/"\
+        "table[@class='tab1']"
+
+
+
     def __init__(self):
-        # Obiekt Session, używany przy kolejnych zapytaniach. Potrzebny, żeby
-        # raz ustawić nagłówki i nie przekazywać ich do każdego zapytania
-        # osobno.
-        self.session = requests.Session()
-        self.session.headers.update({
-            'Accept'            : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Encoding'   : 'gzip, deflate',
-            'Accept-Language'   : 'pl,en-US;q=0.7,en;q=0.3',
-            'Cache-Control'     : 'max-age=0',
-            'Connection'        : 'keep-alive',
-            #'Cookie'            : 'krs_fk45=h5mfc4oblmd1e1nokkpu4694e5; krs_cookie_accepted=true',
-            #'DNT'               : '1',
-            'Host'              : 'www.krs-online.com.pl',
-            #'Referer'           : 'http://www.krs-online.com.pl/muzeum-slaska-opolskiego-krs-1260077.html',
-            'User-Agent'        : 'Mozilla/5.0 (X11; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'
-            })
-
-        # Obiekt parsera, różny od domyślnego tylko w tym, że nie przewraca się
-        # na błędach. Przestrzeń nazw dla XML'i przechowujących strony HTTP.
-        self.parser = etree.XMLParser(recover=True)
-        self.xml_ns = {'ns': 'http://www.w3.org/1999/xhtml'}
+        self.session = Session()
+        self.stripper = re.compile(r'[\W+_]', re.UNICODE)
 
 
 
-    # Parsuje stronę wyników wyszukiwania KRS'u. Dostaje surowy tekst HTML,
-    # zwraca linki do podstron, które są rezultatem wyszukania.
-    def _parseSearchResults(self, text):
-        document = etree.fromstring(text, parser = self.parser)
+    def strip_string(self, text):
+        if not text:
+            return ''
 
-        links = document.findall(".//ns:div[@id='main']/ns:p/ns:a",\
-                namespaces = self.xml_ns)
+        return self.stripper.sub('', text.lower())
+
+    
+    def _geocode_address(self, data):
+        pass
+
+
+    #def _accept_table(self, table):
+    #    pass
+
+    # Parsuje jedną tabelę ze strony podmiotu. Dostaje obiekt '_Element'
+    # odpowiadający tabeli, zwraca listę znalezionych danych (jeśli tabela
+    # miała dwie kolumny). Tu znajdują się też rozpoznania, czy tabela w ogóle
+    # zawiera interesujące dane.
+    # UPDATE: na razie 'akceptowalna' tabelka to taka, która ma 2 kolumny. Nie
+    # jest to nazbyt zmyślne, więc jeśli trzeba będzie czegoś więcej, tu będzie
+    # rozpoznawanie rodzaju table. Odrzucanie -> return None.
+    def _parse_table(self, table_elem):
+
+        # Tak może wyglądać sprawdzanie poprawności tabeli:
+        #if not self._accept_table( table_elem ):
+        #    return None
+
+        data = []
+
+        for row in table_elem:
+            cells = row.getchildren()
+            #print('Children: {}'.format([c.text for c in cells]))
+            if len(cells) == 2:
+                key = cells[0].text
+                val = cells[1].text
+                if key and val:
+                    key = self.strip_string(key)
+
+                    if u'nazwisko' in key and u'imię' in key:
+                        return []
+
+                    data.append( (key,val) )
+        return data
+
+
+
+    # Wyciąga informacje ze strony jednego podmiotu KRS. Dostaje adres strony,
+    # zwraca listę wierszy z danymi.
+    def get_entity(self, address):
+
+        entity_site = self.session.get_site(address)
+        data_tables = entity_site.findall( KRS.path_data_table )
+
+        print('Found {} tables.'.format( len(data_tables) ))
 
         result = []
-        for link in links:
-            result.append('http://www.krs-online.com.pl/{}'.format(link.get('href')))
+        for table in data_tables:
+            parse_result = self._parse_table( table )
+            if parse_result is not None:
+                result.extend( parse_result )
+
+        self._geocode_address( result )
+
         return result
+
+
+    # Parsuje stronę wyników wyszukiwania KRS'u. Dostaje stronę w ElementTree,
+    # zwraca linki do podstron, które są rezultatem wyszukania.
+    def _parse_search_results(self, results_site):
+        links = results_site.findall( KRS.path_search_result )
+
+        get_addr = lambda link:\
+            'http://www.krs-online.com.pl/{}'.format(link.get('href'))
+
+        return map(get_addr,  links)
+
+
+
+    def search_for(self, look_for):
+        search_results = self.session.get_site('http://www.krs-online.com.pl/',
+            params = {'p': 6, 'look': look_for})
+
+        search_results = self._parse_search_results( search_results )
+
+        print('Search results:')
+        for r in search_results: print(r)
+
+        if len(search_results) == 0:
+            raise RuntimeError('Nothing found when searching in KRS for {}'
+                    .format(look_for))
+        elif len(search_results) > 1:
+            print('Warning: searching in KRS for id {} returned more than one '
+                    'result!. First result will be used.'.format(look_for))
+
+        entity = self.get_entity( search_results[0] )
+        return entity
 
 
 
     # Szukanie stron z informacjami o podmiotach wg NIP'u. Zwraca listę linków
     # z wynikami wyszukania.
-    def searchTaxID(self, nip):
-        r = self.session.get('http://www.krs-online.com.pl/',
-            params = {'p': 6, 'look': nip})
-        r.raise_for_status()
-        
-        return self._parseSearchResults(r.text)
+    # TODO Czy te w ogóle będą potrzebne???
+    def get_tax_id(self, nip):
+        pass
+
+    def get_registry_id(self, reg_id):
+        pass
 
 
-
-    # Parsuje jedną tabelę ze strony podmiotu. Dostaje obiekt '_Element'
-    # odpowiadający tabeli, zwraca słownik znalezionych danych (jeśli tabela
-    # miała dwie kolumny). Tu znajdują się też rozpoznania, czy tabela w ogóle
-    # zawiera interesujące dane.
-    # TODO usuwanie niealfanumerycznych znaków z kluczy??
-    def _parseTable(self, table_elem):
-        data = {}
-
-        for row in table_elem:
-            cells = row.getchildren()
-            print('Children: {}'.format([c.text for c in cells]))
-            if len(cells) == 2:
-                key = cells[0].text
-                val = cells[1].text
-                if key and val:
-                    key = key.lower().strip()
-
-                    if key == u'nazwisko i imię:': return {}
-
-                    data[key] = val
-        return data
-
-
-    # Parsuje stronę podmiotu. Dostaje surowy tekst HTML, zwraca słownik
-    # znalezionych w tabelach danych.
-    def _parseEntitySite(self, text):
-        document = etree.fromstring(text, parser = self.parser)
-
-        tables = document.findall(".//ns:div[@id='all']/ns:div[@id='main']/ns:table[@class='tab1']",
-                namespaces = self.xml_ns)
-
-        result = {}
-        for table in tables:
-            result.update(self._parseTable(table))
-        return result
-
-
-
-    # Wyciąga informacje ze strony jednego podmiotu KRS. Dostaje adres strony,
-    # zwraca słownik danych.
-    def extractEntityData(self, address):
-        r = self.session.get(address)
-        r.raise_for_status()
-
-        #document = etree.fromstring(r.text, parser = self.parser)
-        return self._parseEntitySite(r.text)
 
 
 
@@ -116,37 +142,33 @@ class KRS(object):
 def example():
     krs = KRS()
 
-    # Szukanie po NIP'ie.
-    links = krs.searchTaxID(7542527629)
-    
-    # Coś dziwnego? Wyszukiwanie dla NIP'u zwróciło więcej niż jeden wynik?
-    if len(links) != 1:
-        return
+    #tax_id = 7542527629
+    tax_id = 6290011681
 
-    # Parsowanie danych ze zwróconej strony.
-    data = krs.extractEntityData(links[0])
+    # Szukanie po NIP'ie (może też być numer KRS, jest w danych z money.pl)
+    entity_data = krs.search_for(tax_id)
+    
+    # Jeżeli wyszukiwanie zwróci więcej niż jeden wynik, wypisze się ostrzeżenie
+    # i sparsowany zostanie pierwszy z nich.
+    # Jeżeli nic się nie znajdzie, poleci RuntimeError.
 
     # Zrób sobie coś z danymi.
-    for k, v in data.iteritems():
-        print('{} -> {}'.format(k,v))
+    print('Dane podmiotu o NIP\'ie {}:'.format(tax_id))
+    for element in entity_data:
+        print(element)
 
 
 
 def main():
-    
-    example()
-    
-    return
-    
     krs = KRS()
 
     # Testowe parsowanie tekstu strony z pliku.
     #with codecs.open('site', 'r', 'utf8') as src:
-    #    return krs._parseSearchResults(srd.read())
+    #    return krs._parse_search_results(srd.read())
 
 
     # Testowe wyciąganie danych ze strony w pliku.
-    with codecs.open('krs_entity_site', 'r', 'utf8') as src:
+    with codecs.open('./cache/krs_entity_site', 'r', 'utf8') as src:
         tabs = krs._parseEntitySite(src.read())
 
         for k,v in tabs.iteritems():
@@ -154,5 +176,6 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+    example()
 
